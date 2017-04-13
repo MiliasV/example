c2b1aef561d9bf091e49d523477951580069dd17
bill <milias.vasilis@gmail.com>
2017-04-13 17:32:13 +0200

    game installed

--- cards.py
+++ cards.py
@@ -1,11 +1,12 @@
-/usr/bin/python
+#!/usr/bin/python
 import os, random,time
 
-path="/home/applinux/Desktop/Cards/PNG-cards-1.3"
-while 1>0 :
-    rand= random.choice([x for x in os.listdir(path)
+path="Cards" # <== path to the Deck
+while 1>0 :  # <== game is on forever
+    rand = random.choice([x for x in os.listdir(path)
                    if os.path.isfile(os.path.join(path, x))])
-    bash="eog --single-window"+" "+path+"/"+rand+" &"
-    print path+rand
+    bash="eog --single-window" + " " + path + "/" + rand + " &"
+    print path + rand
     os.system(bash)
-    time.sleep(3)
+    randTime = random.choice(range(1,120))
+    time.sleep(randTime)
